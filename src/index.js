6f427c48a3c7d326c48a10f496932003ea538441
Enes Furkan Kaya <furkan@medicasimple.com>
2025-05-25 22:28:07 +0200

    basic ai detection

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 const http = require('http')
 const fs = require('fs')
 const path = require('path')
+const isBot = require('./functions/BotDetectionByHeader')
 
 const server = http.createServer((req, res) => {
+  const userAgent = req.headers['user-agent'] || ''
+
+  if (isBot(userAgent) || hasSuspiciousHeaders(req.headers)) {
+    res.writeHead(302, { Location: '/furkan' })
+    res.end()
+    return
+  }
+
+  /* 
+
+  // client-side POST check via JavaScript
+  <scrpit>
+    fetch('/check-post', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ test: 'test' })
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error))
+    </script>
+  */
+
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' })
     res.end('Welcome to My App')
